aa906e496f5c4b91b4f8f8927baf3830e9694305
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-09-02 14:51:56 +0800

    Initial commit

--- src/rune.ts
+++ src/rune.ts
@@ -1,7 +1,27 @@
-let lessThen4 = 0, lessThen5 = 0;
+let lessThen4 = 0, lessThen5 = 0, result = 0;
+const results: number[] = [];
+
+const newTest = (log: string, fn: () => void): void => {
+    console.log(log);
+    lessThen4 = 0;
+    lessThen5 = 0;
+    results.length = 0;
+    for (let i = 0; i < 10000; i++) {
+        result = 0;
+        fn();
+        results.push(result);
+    }
+    results.sort((a, b) => a - b);
+    console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
+    console.log("95%置信区间：", results.at(250), "~", results.at(-250));
+};
+
+const getColor = (v: number) => v % 5;
+const getCharacter = (v: number) => v % 8;
 
 // 随机符石的个数
-const randCount = (): number => {
+const randLine = (): number[] => {
+    result++;
     let count: number;
     if (lessThen5 >= 19) {
         count = 5;
@@ -27,207 +47,53 @@ const randCount = (): number => {
             lessThen5++;
             lessThen4++;
     }
-    return count;
+    const line = Array.from({length: count}, () => Math.floor(Math.random() * 55));
+    line.sort((a, b) => a - b);
+    return line
 };
 
-// 随机两个完全相同的符石
-const randTwoSame = (): number => {
-    const count = randCount();
-    const result: number[] = [];
-    for (let i = 0; i < count; i++) {
-        const v = Math.floor(Math.random() * 55);
-        if (v < 15) continue;
-        if (result.includes(v)) return v;
-        result.push(v);
-    }
-    return -1;
+const findTwoSame = (arr: number[]) => arr.find((v, index, arr) => index > 0 && v === arr[index - 1]);
+
+const newTestTwoSame = (log: string, predicate: (v: number) => boolean): void => {
+    newTest(log, () => {
+        let cur = [0, 0, 0];
+        for (let i = 0; i < 3; i++) {
+            while (true) {
+                const v = findTwoSame(randLine());
+                if (v && predicate(v)) { // 随出任意两个相同有字符石
+                    cur[i] = v;
+                    if (i < 2 || cur[2] !== cur[0] && cur[2] !== cur[1]) {
+                        if (i == 2 && cur[0] !== cur[1]) {
+                            // 第三个和前两个有一个相同，但前两个不同，重新随一个相同的
+                            let v: number | undefined;
+                            do {
+                                v = findTwoSame(randLine());
+                            } while (!v || v !== cur[2]);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    });
 };
 
-// 随机两个中庸符石
-const randTwoNoNumber = (): boolean => {
-    const count = randCount();
-    let hasNoNumber = false;
-    for (let i = 0; i < count; i++) {
-        const v = Math.floor(Math.random() * 55);
-        if (v >= 15) continue;
-        if (hasNoNumber) return true;
-        hasNoNumber = true;
-    }
-    return false;
+const newTestSpecific = (log: string, predicate: (v: number) => boolean): void => {
+    newTest(log, () => {
+        for (let i = 0; i < 3; i++) {
+            while (true) {
+                let count = 0;
+                for (const v of randLine()) {
+                    if (predicate(v)) count++;
+                }
+                if (count >= 2) break;
+            }
+        }
+    });
 };
-
-// 随机两个同色（有字）符石
-const randTwoSameColor = (mod: number = 5): number => {
-    const count = randCount();
-    const result: number[] = [];
-    for (let i = 0; i < count; i++) {
-        const v = Math.floor(Math.random() * 55);
-        if (v < 15) continue;
-        const color = v % mod;
-        if (result.includes(color)) return color;
-        result.push(color);
-    }
-    return -1;
-};
-
-console.log("===========六个完全相同的符石===========");
-const results: number[] = [];
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    let cur = [0, 0, 0];
-    do {
-        result++;
-        cur[0] = randTwoSame();
-    } while (cur[0] <= 0);
-    do {
-        result++;
-        cur[1] = randTwoSame();
-    } while (cur[1] <= 0);
-    do {
-        result++;
-        cur[2] = randTwoSame();
-    } while (cur[2] != cur[0] && cur[2] != cur[1]);
-    if (cur[0] != cur[1]) {
-        let v = 0;
-        do {
-            result++;
-            v = randTwoSame();
-        } while (v != cur[2]);
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
-
-lessThen4 = 0;
-lessThen5 = 0;
-results.length = 0;
-console.log("===========六金===========");
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    let cur = [0, 0, 0];
-    do {
-        result++;
-        cur[0] = randTwoSameColor();
-    } while (cur[0] < 0);
-    do {
-        result++;
-        cur[1] = randTwoSameColor();
-    } while (cur[1] < 0);
-    do {
-        result++;
-        cur[2] = randTwoSameColor();
-    } while (cur[2] != cur[0] && cur[2] != cur[1]);
-    if (cur[0] != cur[1]) {
-        let v = 0;
-        do {
-            result++;
-            v = randTwoSameColor();
-        } while (v != cur[2]);
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
-
-lessThen4 = 0;
-lessThen5 = 0;
-results.length = 0;
-console.log("===========六乾===========");
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    let cur = [0, 0, 0];
-    do {
-        result++;
-        cur[0] = randTwoSameColor(8);
-    } while (cur[0] <= 0);
-    do {
-        result++;
-        cur[1] = randTwoSameColor(8);
-    } while (cur[1] <= 0);
-    do {
-        result++;
-        cur[2] = randTwoSameColor(8);
-    } while (cur[2] != cur[0] && cur[2] != cur[1]);
-    if (cur[0] != cur[1]) {
-        let v = 0;
-        do {
-            result++;
-            v = randTwoSameColor(8);
-        } while (v != cur[2]);
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
-
-lessThen4 = 0;
-lessThen5 = 0;
-results.length = 0;
-console.log("===========六金乾===========");
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    let cur = 0;
-    for (let j = 0; j < 3; j++) {
-        do {
-            result++;
-            cur = randTwoSame();
-        } while (cur != 54);
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
-
-lessThen4 = 0;
-lessThen5 = 0;
-results.length = 0;
-console.log("===========六金同字===========");
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    let cur = [0, 0, 0];
-    do {
-        result++;
-        cur[0] = randTwoSame();
-    } while (cur[0] <= 0 || cur[0] % 5);
-    do {
-        result++;
-        cur[1] = randTwoSame();
-    } while (cur[1] <= 0 || cur[1] % 5);
-    do {
-        result++;
-        cur[2] = randTwoSame();
-    } while (cur[2] != cur[0] && cur[2] != cur[1]);
-    if (cur[0] != cur[1]) {
-        let v = 0;
-        do {
-            result++;
-            v = randTwoSame();
-        } while (v != cur[2]);
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
-
-lessThen4 = 0;
-lessThen5 = 0;
-results.length = 0;
-console.log("===========六中庸===========");
-for (let i = 0; i < 10000; i++) {
-    let result = 0;
-    for (let j = 0; j < 3; j++) {
-        do {
-            result++;
-        } while (!randTwoNoNumber());
-    }
-    results.push(result);
-}
-results.sort((a, b) => a - b);
-console.log("平均值：", results.reduce((a, b) => a + b, 0) / 10000);
-console.log("95%置信区间：", results.at(250), "~", results.at(-250));
+newTestTwoSame("===========六个完全相同的符石===========", v => v >= 15);
+newTestSpecific("===========六金===========", v => v >= 15 && getColor(v) === 0);
+newTestSpecific("===========六乾===========", v => v >= 15 && getCharacter(v) === 0);
+newTestSpecific("===========六金乾===========", v => v === 15);
+newTestTwoSame("===========六金同字===========", v => v >= 15 && getColor(v) === 0);
+newTestSpecific("===========六中庸===========", v => v < 15);
